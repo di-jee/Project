2b4d1f081fc8bad1269d02258e940505647a3389
di-jee <96427831+di-jee@users.noreply.github.com>
2021-12-25 12:34:26 +0200

    Delete telegram directory

--- telegram/config.py
+++
@@ -1,6 +0,0 @@
-
-token = '1053823998:AAFL5CN1AUzo93etGi_Gu9C2SmIIo8qhclg'
-track_product_database = 'database/products/product_database.db'
-track_database_users = 'database/users/users_database.db'
-table_track_database_users = 'users'
-shelve_way = 'shelve/shelve.db'
