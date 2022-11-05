69465a453976109cbd9e50cd49fe2360873447f9
di-jee <96427831+di-jee@users.noreply.github.com>
2022-11-05 13:17:57 +0300

    Update class_user.py

--- class_user.py
+++ class_user.py
@@ -1,67 +1,56 @@
 class User:
 
-	def __init__(self, id_user, name, balance: int):
-		self._id_user = id_user
-		self._name = name
-		if balance < 0:
-			raise ValueError(f'Balance > 0')
-		self._balance = balance # запрос к базе данных
+	def __init__(self, id_user, balance=0):
+		self.__id_user = id_user
+		self.__balance = balance # get from database
+
+	def __del__(self):
+		print('close database')
 
 	@classmethod
-	def __get_money_add(cls, oth):
-		if not isinstance(oth, (int, User)):
-			raise ValueError('Other don`t int or Human')
+	def __check_other(cls, other, balance=None):
+		# проверяем что other это int
+		if not isinstance(other, int):
+			raise TypeError(f'Other not int: {other}')
 
-		# если oth явл. экземпляром класса Human, то берем его баланс
-		# этим мы можем реализовать перевод денег от одного человека к другому
-		money = oth
-		if isinstance(oth, User):
-			# достаем деньги другого Human
-			money = oth._balance
-			# минусуем их у него, мы же переводим их
-			oth._balance -= money
-		# возвращаем кол-во денег юзера
-		return money
+		# проверка для вычитания
+		# если other > balance, то на балансе недостаточно средств
+		if balance != None and other > balance:
+			if other > balance:
+				raise ValueError(f'Other < balance: {other}')
 
-	@classmethod
-	def __get_money_sub(cls, oth, balance):
-		# проверяем что пользователь ввел число
-		if not isinstance(oth, int):
-			raise ValueError('Other don`t int')
-
-		# проверяем что бы число не привышало баланс
-		if balance < oth:
-			raise ValueError(f'you don`t have enough money: {balance}')
-
-		return oth
+		return other
 
 	# сложение
 	def __add__(self, other):
-		money = self.__get_money_add(other)
-		# возвращаем новый объект с новым балансом
-		return User(self._id_user, self._name, self._balance + money)
+		money = self.__check_other(other)
+		self.__balance += money
 
-	# если переменные были прописаны в другом порядке
+		# не предпологается сложение или вычитание объектов типа User
+		# по-этому возвращаем этот же экземпляр
+		return self
+
 	def __radd__(self, other):
 		return self + other
 
-	# метод для сокращения +=
 	def __iadd__(self, other):
-		money = self.__get_money_add(other)
-		self._balance += money
+		money = self.__check_other(other)
+		self.__balance += money
 
 		return self
 
 	# вычитание
 	def __sub__(self, other):
-		money = self.__get_money_sub(other, self._balance)
-		return User(self._id_user, self._name, self._balance - money)
+		money = self.__check_other(other, self.__balance)
+		self.__balance -= money
+
+		return self
 
 	def __rsub__(self, other):
 		return self - other
 
 	def __isub__(self, other):
-		money = self.__get_money_sub(other, self._balance)
-		self._balance -= money
+		money = self.__check_other(other, self.__balance)
+		self.__balance -= money
 
 		return self
